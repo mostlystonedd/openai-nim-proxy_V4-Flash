e1e15bebbeebd05d765b05b48b2796c616caaa8a
mostlystonedd <fathersson6@gmail.com>
2026-05-04 16:00:12 +0530

    Update CORS configuration in server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.use(express.json());
 
 const NIM_API_BASE = process.env.NIM_API_BASE || 'https://integrate.api.nvidia.com/v1';
